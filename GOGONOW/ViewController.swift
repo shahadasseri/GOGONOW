a88e954c6439415719210d0b48a60f20010fcc19
shahad <shadasseri@gmail.com>
2021-10-22 13:42:36 +0300

    Initial Commit

---
+++ GOGONOW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GOGONOW
+//
+//  Created by shahad nasser asseri on 16/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
